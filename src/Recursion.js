7e7fc45ee5f6f142f31225a7c835d00fe31ada5d
Mike Ingber <mbingber@gmail.com>
2018-07-08 21:48:56 -0400

    better centering

--- src/Recursion.js
+++ src/Recursion.js
@@ -8,11 +8,13 @@ export default class Recursion extends Component {
     numSquares: PropTypes.number,
     index: PropTypes.number,
     sideLength: PropTypes.number,
+    translate: PropTypes.number,
   };
 
   static defaultProps = {
     index: 0,
     sideLength: 600,
+    translate: 0,
   };
 
   get nextSideLength() {
@@ -21,33 +23,38 @@ export default class Recursion extends Component {
     return ratio * this.props.sideLength;
   }
 
+  get nextTranslate() {
+    return (this.props.sideLength - this.nextSideLength) / 2;
+  }
+
   get rotationAngle() {
     const { index, scale } = this.props;
     return index > 0 ? Math.PI/4 - Math.atan(1 - scale) : 0;
   }
 
   get styles() {
-    const { index, sideLength, numSquares } = this.props;
+    const { index, sideLength, numSquares, translate } = this.props;
 
     const percent = Math.round((index / numSquares) * 100);
     const dim = `${Math.round(sideLength)}px`;
-    const transform = `rotate(${this.rotationAngle * 180 / Math.PI}deg)`;
-
+    const translation = `translateX(${Math.round(translate)}px) translateY(${Math.round(translate)}px)`;
+    const rotation = `rotate(${this.rotationAngle * 180 / Math.PI}deg)`;
+    
     return {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      // display: 'flex',
+      // alignItems: 'center',
+      // justifyContent: 'center',
       backgroundColor: `hsl(0, 0%, ${percent}%)`,
       height: dim,
       width: dim,
-      transform,
+      transform: `${translation} ${rotation}`,
     };
   }
 
   render = () => {
     const { index, numSquares, ...rest } = this.props;
 
-    if (index > numSquares) return null;
+    if (index >= numSquares) return null;
 
     return (
       <div style={this.styles}>
@@ -55,6 +62,7 @@ export default class Recursion extends Component {
           {...rest}
           index={index+1}
           sideLength={this.nextSideLength}
+          translate={this.nextTranslate}
           numSquares={numSquares}
         />
       </div>
